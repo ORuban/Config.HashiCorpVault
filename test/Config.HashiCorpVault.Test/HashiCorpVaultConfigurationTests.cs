f9f557e9337ac00d7fe2989610e284209e4ba9d1
oruban <settlers.net@gmail.com>
2018-02-11 16:06:52 +0200

    add test project

---
+++ test/Config.HashiCorpVault.Test/HashiCorpVaultConfigurationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ORuban.Extensions.Configuration.HashiCorpVault.Test
+{
+    public class HashiCorpVaultConfigurationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
