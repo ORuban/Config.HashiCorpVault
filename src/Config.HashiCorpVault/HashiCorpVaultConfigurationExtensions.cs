ffb9fd3eaba5a2c9889e853c26b2fc0fdf296d4e
oruban <settlers.net@gmail.com>
2018-02-18 17:00:52 +0200

    removed client's implementation based on VaultSharp, left only abstraction's level

--- src/Config.HashiCorpVault/HashiCorpVaultConfigurationExtensions.cs
+++ src/Config.HashiCorpVault/HashiCorpVaultConfigurationExtensions.cs
@@ -8,51 +8,6 @@ namespace ORuban.Extensions.Configuration.HashiCorpVault
     {
         public static IConfigurationBuilder AddHashiCorpVault(
             this IConfigurationBuilder configurationBuilder,
-            string vaultAddressWithPort,
-            string token,
-            string prefix,
-            IEnumerable<string> secrets)
-        {
-            if (string.IsNullOrWhiteSpace(vaultAddressWithPort))
-            {
-                throw new ArgumentException("Vault Address is required", nameof(vaultAddressWithPort));
-            }
-
-            if (string.IsNullOrEmpty(token))
-            {
-                throw new ArgumentException("Auth token is required", nameof(token));
-            }
-
-            var client = new HashiCorpVaultClientWrapper(vaultAddressWithPort, token);
-
-            return AddHashiCorpVault(configurationBuilder, client, prefix, secrets);
-        }
-
-        public static IConfigurationBuilder AddHashiCorpVault(
-            this IConfigurationBuilder configurationBuilder,
-            string vaultAddressWithPort,
-            string roleId, 
-            string secretId,
-            string prefix,
-            IEnumerable<string> secrets)
-        {
-            if (string.IsNullOrWhiteSpace(vaultAddressWithPort))
-            {
-                throw new ArgumentException("Vault Address is required", nameof(vaultAddressWithPort));
-            }
-
-            if (string.IsNullOrEmpty(roleId))
-            {
-                throw new ArgumentException("Role Id is required", nameof(roleId));
-            }
-
-            var client = new HashiCorpVaultClientWrapper(vaultAddressWithPort, roleId, secretId);
-
-            return AddHashiCorpVault(configurationBuilder, client, prefix, secrets);
-        }
-
-        public static IConfigurationBuilder AddHashiCorpVault(
-            this IConfigurationBuilder configurationBuilder,
             IHashiCorpVaultClient client,
             string prefix,
             IEnumerable<string> secrets)
