6825000e5a12d5749aa8ad472f5cfc25fd82298b
jincod <vadim.abdrashitov@gmail.com>
2018-09-11 11:07:05 +0500

    updated packages

--- sample/ConsoleApp/HashiCorpVaultClientWrapper.cs
+++ sample/ConsoleApp/HashiCorpVaultClientWrapper.cs
@@ -1,27 +1,27 @@
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using ORuban.Extensions.Configuration.HashiCorpVault;
 using VaultSharp;
-using VaultSharp.Backends.Authentication.Models;
-using VaultSharp.Backends.Authentication.Models.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
 
 namespace ConsoleApp
 {
     public class HashiCorpVaultClientWrapper : IHashiCorpVaultClient
     {
-        private IVaultClient _vaultClientImplementation;
+        private readonly IVaultClient _vaultClientImplementation;
 
         public HashiCorpVaultClientWrapper(string vaultAddressWithPort, string token)
         {
-            IAuthenticationInfo authenticationInfo = new TokenAuthenticationInfo(token);
-            _vaultClientImplementation = VaultClientFactory.CreateVaultClient(new Uri(vaultAddressWithPort), authenticationInfo);
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo(token);
+            var vaultClientSettings = new VaultClientSettings(vaultAddressWithPort, authMethod);
+            _vaultClientImplementation = new VaultClient(vaultClientSettings);
         }
 
         public async Task<string> GetSecretAsync(string path)
         {
-            var rawData = await _vaultClientImplementation.ReadSecretAsync(path);
-            return rawData.Data.Values.First().ToString();
+            var rawData = await _vaultClientImplementation.V1.Secrets.KeyValue.V2.ReadSecretAsync(path);
+            return rawData.Data.Data.First().Value.ToString();
         }
     }
 }
--- sample/ConsoleApp/Program.cs
+++ sample/ConsoleApp/Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using VaultSharp;
-using VaultSharp.Backends.Authentication.Models;
-using VaultSharp.Backends.Authentication.Models.Token;
 using ORuban.Extensions.Configuration.HashiCorpVault;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //start Vault Server in dev mode
             // ./vault server -dev
@@ -19,46 +19,51 @@ namespace ConsoleApp
             string vaultAddressWithPort = "http://127.0.0.1:8200";
             string token = "75a244db-9164-4130-db60-50c7081f50ba";
 
-            System.Console.WriteLine("Creating Vault client");
-            System.Console.WriteLine($"Vault Address: {vaultAddressWithPort}");
-            System.Console.WriteLine($"Auth Token: {token}");
+            Console.WriteLine("Creating Vault client");
+            Console.WriteLine($"Vault Address: {vaultAddressWithPort}");
+            Console.WriteLine($"Auth Token: {token}");
 
-            IAuthenticationInfo tokenAuthenticationInfo = new TokenAuthenticationInfo(token);
-            var vaultClient = VaultClientFactory.CreateVaultClient(new Uri(vaultAddressWithPort), tokenAuthenticationInfo);
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo(token);
+            var vaultClientSettings = new VaultClientSettings(vaultAddressWithPort, authMethod);
+            var vaultClient = new VaultClient(vaultClientSettings);
 
-            System.Console.WriteLine("Writing data to Vault...");
-            var resulA = vaultClient.WriteSecretAsync("secret/keyA", new Dictionary<string, object>(){
+            Console.WriteLine("Writing data to Vault...");
+            await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync("secret/keyA", new Dictionary<string, object>
+            {
                 {"prop1", "valueA"}
-            }).Result;
+            });
 
-            var resultB = vaultClient.WriteSecretAsync("secret/keyB", new Dictionary<string, object>(){
+            await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync("secret/keyB", new Dictionary<string, object>
+            {
                 {"prop1", "valueB"}
-            }).Result;
+            });
 
-            var resultC = vaultClient.WriteSecretAsync("secret/group1/keyC", new Dictionary<string, object>(){
+            await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync("secret/group1/keyC", new Dictionary<string, object>
+            {
                 {"prop1", "valueC"}
-            }).Result;
+            });
 
-            var resultD = vaultClient.WriteSecretAsync("secret/group2/keyD", new Dictionary<string, object>(){
+            await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync("secret/group2/keyD", new Dictionary<string, object>
+            {
                 {"prop1", "valueD"}
-            }).Result;
+            });
 
-             var client = new HashiCorpVaultClientWrapper(vaultAddressWithPort, token);
+            var client = new HashiCorpVaultClientWrapper(vaultAddressWithPort, token);
 
-            IConfigurationBuilder configBuilder =
+            var configBuilder =
                 new ConfigurationBuilder()
-                .AddHashiCorpVault(client, "secret", new[] { "keyA", "keyB", "group1/keyC" })
-                .AddHashiCorpVault(client, "secret/group2", new[] { "keyD" });
+                    .AddHashiCorpVault(client, "secret", new[] {"keyA", "keyB", "group1/keyC"})
+                    .AddHashiCorpVault(client, "secret/group2", new[] {"keyD"});
 
-            System.Console.WriteLine("Building configuration...");
+            Console.WriteLine("Building configuration...");
 
             IConfiguration config = configBuilder.Build();
 
-            System.Console.WriteLine("The following settings populated:");
+            Console.WriteLine("The following settings populated:");
 
             foreach (var item in config.AsEnumerable())
             {
-                System.Console.WriteLine(item);
+                Console.WriteLine(item);
             }
         }
     }
